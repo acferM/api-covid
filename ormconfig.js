0fdc41136ef6d07900c9b695ade8353965bb4291
acferM <matheus.acfer@nave.org.br>
2021-05-20 11:37:48 -0300

    Procfile created, update ormconfing.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,20 @@
 module.exports = {
-    "url": process.env.DATABASE_URL,
-    "name": "default",
-    "type": "postgres",
-    "entities": [
-      "./dist/modules/**/infra/typeorm/entities/*.js"
-    ],
-    "migrations": [
-      "./dist/shared/infra/typeorm/migrations/*.js"
-    ],
-    "cli": {
-      "migrationsDir": "./src/shared/infra/typeorm/migrations"
-    },
-    "ssl": true,
-    "extra": {
-      "ssl": {
-        "rejectUnauthorized": false
-      }
+  "url": process.env.DATABASE_URL,
+  "name": "default",
+  "type": "postgres",
+  "entities": [
+    "./dist/modules/**/infra/typeorm/entities/*.js"
+  ],
+  "migrations": [
+    "./dist/shared/infra/typeorm/migrations/*.js"
+  ],
+  "cli": {
+    "migrationsDir": "./src/shared/infra/typeorm/migrations"
+  },
+  "ssl": true,
+  "extra": {
+    "ssl": {
+      "rejectUnauthorized": false
     }
   }
 }
